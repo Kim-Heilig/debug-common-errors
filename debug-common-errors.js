0b7b91915d46e47507c1869a11cb382d102412f5
Kim Heilig <kimmarieheilig@gmail.com>
2025-07-18 19:33:43 -0400

    completed assignment

--- debug-common-errors.js
+++ debug-common-errors.js
@@ -24,24 +24,25 @@ Think about which debugging methods you found most useful and how you might appl
 // Description:
 // This program is intended to display a simple prompt in the console but fails to run.
 
-console.log("Welcome to the bootcamp
+console.log("Welcome to the bootcamp");
 
 // What’s Wrong?
 
+
+// I can see from the color indication on vs code that there is an issue with the last part of bootcamp.  The p is red (rest of the word is yellow and there is a wiggly red underline indicating a problem.  Clearly it is missing the " and the ) and the ;
 
 // Program B
 // Description:
 // This code attempts to multiply each number in an array by 2 and display the results. However, it crashes at runtime.
 
-let numbers = [2, 4, "eight"];
+let numbers = [2, 4, 8];
 for (let i = 0; i < numbers.length; i++) {
   let doubled = numbers[i] * 2;
   console.log(doubled);
 }
 
 // What’s Wrong?
-
-
+// "eight" is NaN and can not be multiplied by 2.  This is a logic error.  It will be fixed by replacing it with "8"
 
 // Program C (Logic Error)
 // Description:
@@ -51,12 +52,14 @@ function isPrime(num) {
   if (num < 2) return false;
   for (let i = 2; i < num; i++) {
     if (num % i === 0) {
-      return true;  // Supposed to indicate num is NOT prime
+      return false;  // Supposed to indicate num is NOT prime
     }
   }
-  return false; // Supposed to indicate num IS prime
+  return true; // Supposed to indicate num IS prime
 }
 
 console.log(isPrime(7)); // Expected true but gets false
 
 // What’s Wrong?
+
+//You want the loop to test all numbers less than the target number to see if any of those numbers are divisible.  If every number up until that number is divisible evenly, this would indicate it is not a prime.  So you want it to return false (the original function has it returning true).  Therefore, I switched the return statements around so that if it is divisible by numbers less than itself, it is false (not prime).  If it isn't, it returns true (prime).
